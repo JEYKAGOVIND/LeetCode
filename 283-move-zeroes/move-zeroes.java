97afe642296bd972b7aeaeaa69c0b80bce06b750
JEYA KARTIKA GOVINDARAJAN <jeykagovind@gmail.com>
2026-02-02 12:13:55 +0530

    Time: 2 ms (90.02%) | Memory: 47.8 MB (70.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,8 +5,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             
                 if(nums[i]!=0){
-                    int temp;
-                    temp=nums[i];
+                
+                    int temp=nums[i];
                     nums[i]=nums[j];
                     nums[j]=temp;
                     j++;
