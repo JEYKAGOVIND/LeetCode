b1cbbf2d071237ff74477c3c4cefc48020bc6990
JEYA KARTIKA GOVINDARAJAN <jeykagovind@gmail.com>
2026-02-05 14:41:27 +0530

    Time: 24 ms (63.51%) | Memory: 65.3 MB (88.93%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -3,17 +3,17 @@ class Solution {
         int n = nums.length;
         Map<Integer, Integer> mp = new HashMap<>();
         int sum = 0;
-        int subArrayLength = 0;
+        int subarrlen = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i] == 0 ? -1 : 1;
             if (sum == 0) {
-                subArrayLength = i + 1;
+                subarrlen = i + 1;
             } else if (mp.containsKey(sum)) {
-                subArrayLength = Math.max(subArrayLength, i - mp.get(sum));
+                subarrlen= Math.max(subarrlen, i - mp.get(sum));
             } else {
                 mp.put(sum, i);
             }
         }
-        return subArrayLength;
+        return subarrlen;
     }
 }
