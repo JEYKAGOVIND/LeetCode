686270875dd3c48e5eaac0eeb7ffbba3ed8dc494
JEYA KARTIKA GOVINDARAJAN <jeykagovind@gmail.com>
2026-07-23 11:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (88.52%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,23 @@
+/*class Solution {
+    public void reverseString(char[] s) {
+        StringBuilder build=new StringBuilder(new String (s));
+        build.reverse();
+        for(int i=0;i<s.length;i++){
+            s[i]=build.charAt(i);
+        }
+        
+    }
+}*/
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0;
+        int right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
